9ea5175d9ff187e937dedc4348a5c7a55c7c1a92
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-12 13:14:32 +0100

    Eliminado Callable redundante.
    Lógica de negocio extraída a método estático interfaz

--- feedback2/src/main/java/com/yuanyuanis/concurrente/feedback2/completableFuture/copiarYZippear/ZipAndCopy.java
+++ feedback2/src/main/java/com/yuanyuanis/concurrente/feedback2/completableFuture/copiarYZippear/ZipAndCopy.java
@@ -1,8 +1,45 @@
 package com.yuanyuanis.concurrente.feedback2.completableFuture.copiarYZippear;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public interface ZipAndCopy {
 
-    void zipearYCopiar(String origenPath, String destinoPath);
+    void zippearYCopiar(String origenPath, String destinoPath);
+
+    /**
+     * Método que recibe una ruta origen y destino, comprime en zip y devuelve la ruta
+     * devuelve la ruta destino donde queda el archivo comprimido
+     * @param origenPath
+     * @param destinoPath
+     * @return Path
+     */
+    public static Path getZipAbsoluteName(String origenPath, String destinoPath) {
+        Path origen = Paths.get(origenPath);
+        Path destino = Paths.get(destinoPath);
+
+        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(destino))) {
+            Files.walk(origen)
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        ZipEntry zipEntry = new ZipEntry(origen.relativize(path).toString());
+                        try {
+                            zos.putNextEntry(zipEntry);
+                            Files.copy(path, zos);
+                            zos.closeEntry();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return destino;
+    }
 
 
 }
