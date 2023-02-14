09b13eabb05b6203a97d42148693660004c10892
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-14 17:27:58 +0100

    Arreglado module-info para que apunte a v3, exportado controller

--- restCountriesV3/src/main/java/module-info.java
+++ restCountriesV3/src/main/java/module-info.java
@@ -3,12 +3,13 @@ module com.yuanyuanis.concurrente.restcountriesv3 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires org.controlsfx.controls;
     requires retrofit2;
     requires converter.gson;
     requires java.sql;
 
     opens com.yuanyuanis.concurrente.restcountriesv3 to javafx.fxml;
-    opens com.yuanyuanis.concurrente.restcountries.domain;
+    opens com.yuanyuanis.concurrente.restcountriesv3.controller to javafx.fxml;
+    opens com.yuanyuanis.concurrente.restcountriesv3.domain;
     exports com.yuanyuanis.concurrente.restcountriesv3;
+    exports com.yuanyuanis.concurrente.restcountriesv3.controller;
 }
