ead7f46230e7584e0670448ad533a716ff5a7854
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-14 14:35:04 +0100

    Adañdidas a module info required necesarios
    retrofit, gson y java-sql???(debido a versiones  viejas de gson)

--- RestCountries/src/main/java/module-info.java
+++ RestCountries/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.yuanyuanis.concurrente.restcountries {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires retrofit2;
+    requires converter.gson;
+    requires java.sql;
+
     requires org.controlsfx.controls;
 
     opens com.yuanyuanis.concurrente.restcountries to javafx.fxml;
