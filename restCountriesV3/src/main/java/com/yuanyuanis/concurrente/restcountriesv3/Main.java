bc41a9928d103eb862e88bebc3701072f1ab1dcb
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-14 17:25:22 +0100

    Añadido main para ejecutar la operacion

---
+++ restCountriesV3/src/main/java/com/yuanyuanis/concurrente/restcountriesv3/Main.java
@@ -0,0 +1,23 @@
+package com.yuanyuanis.concurrente.restcountriesv3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("country.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
