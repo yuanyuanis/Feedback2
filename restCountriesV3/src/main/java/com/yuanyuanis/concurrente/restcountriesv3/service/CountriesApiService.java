2920fff149cd9a25b860dfda21c85f7c9c82dfff
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-15 10:12:32 +0100

    Fixed 404 bad url

--- restCountriesV3/src/main/java/com/yuanyuanis/concurrente/restcountriesv3/service/CountriesApiService.java
+++ restCountriesV3/src/main/java/com/yuanyuanis/concurrente/restcountriesv3/service/CountriesApiService.java
@@ -3,19 +3,18 @@ package com.yuanyuanis.concurrente.restcountriesv3.service;
 
 import com.yuanyuanis.concurrente.restcountriesv3.domain.Country;
 
-import rx.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import rx.Observable;
 
 import java.util.List;
 
 
 public interface CountriesApiService {
 
-    @GET("/rest/v2/all")
+    @GET("/v2/all")
     Observable<List<Country>> getAllCountries();
 
-    // Cuidado! Aunque la respuesta en un solo país viene en forma de array
-    @GET("/rest/v2/name/{name}")
+    @GET("/v2/name/{name}")
     Observable<List<Country>> getCountry(@Path("name") String name);
 }
