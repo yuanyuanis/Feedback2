269dc50e2e321180d964c2e69bf9edb58e02ddb2
yuanyuanis <juanibanezblanco@gmail.com>
2023-02-15 12:57:21 +0100

    Added wrapper to module

--- openFoodFactsJavaFX/src/main/java/module-info.java
+++ openFoodFactsJavaFX/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.yuanyuanis.openfoodfactsjavafx {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires openfoodfacts.java.wrapper;
 
     opens com.yuanyuanis.openfoodfactsjavafx to javafx.fxml;
     exports com.yuanyuanis.openfoodfactsjavafx;
